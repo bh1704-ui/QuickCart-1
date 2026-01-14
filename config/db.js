1a2ef243b7fcf191c8c618516855b40df2e7b4e2
bh1704-ui <bharath.p.1704@gmail.com>
2026-01-14 19:10:54 +0530

    fix(env): corrected MongoDB URI and environment variables-3

--- config/db.js
+++ config/db.js
@@ -18,7 +18,8 @@ async function connectDB(){
         }
 
 
-    cached.promise =mongoose.connect('${process.env.MONGODB_URI}/quickcart',opts).then( mongoose => {
+    cached.promise =mongoose.connect(`${process.env.MONGODB_URI}/quickcart`,opts).
+    then( mongoose => {
         return mongoose
     })
 
